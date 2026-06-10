3d4adc1cc9e169906e5f19e33cb178d271e1c030
shaadali-tech <215ics043@gbu.ac.in>
2026-06-10 20:11:53 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
